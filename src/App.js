e7a29952e5ae28ae3f42f913fc3ffcd9d9e2a603
robinsrk <robinsrk3@gmail.com>
2021-03-19 10:03:42 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Tickets from "./components/Tickets/Tickets";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home" style={{ backgroundColor: "#282a36" }}>
+      <Header></Header>
+      <Tickets></Tickets>
+      <Router>
+        <Switch>
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
